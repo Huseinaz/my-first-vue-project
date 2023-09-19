c373f6907f302c817dd75f12daad1b80d3772072
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-09-19 12:26:51 +0300

    Script Setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '@/components/TheWelcome.vue'
+import swal from 'sweetalert'
+
+// eslint-disable-next-line no-undef
+let message = $ref("Hello, World!");
+
+setTimeout(() =>{
+  message = "I Have Been Changed";
+},2000);
+
+let doSomething = () => {
+  swal("doing it now");
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message" />
+      <button @click="doSomething">CLick Me</button>
+    </p>
   </main>
 </template>
